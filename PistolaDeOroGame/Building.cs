f5060e802eef430ef3ee5de1c288423d196fb9de
Alexandro Cazares <CazaresCode@gmail.com>
2021-05-12 16:19:02 -0400

    added the buildings in the UI in the field

--- PistolaDeOroGame/Building.cs
+++ PistolaDeOroGame/Building.cs
@@ -6,104 +6,78 @@ using System.Threading.Tasks;
 
 namespace PistolaDeOroGame
 {
-    public abstract class Building
+    public class Building
 
     {
-        /// <summary> For Randomizer  </summary>
-        public int Rooms { get; } 
+        /// <summary> What the building is connected to.</summary>
         public List<string> Exits {get;}
+        /// <summary> The lobby is the first room the user enters. This would leave to another room, or rooms. So there is a "cw" stating the possible options on what to do or where to go.</summary>
         public string Lobby { get; }
-
-        public Building(int rooms, List<string> exits, string lobby)
+        public Building( string lobby, List<string> exits)
         {
-            Rooms = rooms;
             Exits = exits;
             Lobby = lobby;
         }
     }
-    public class Saloon : Building
-    {
-        /// <summary> For Randomizer  </summary>
-        public int Rooms { get; }
-        public List<string> Exits { get; }
-        public string Lobby { get; }
 
-        public Saloon (int rooms, List<string> exits, string lobby)
-        {
-            Rooms = rooms;
-            Exits = exits;
-            Lobby = lobby;
-        }
-    }
-    public class TradingPost : Building
-    {
-        /// <summary> For Randomizer  </summary>
-        public int Rooms { get; }
-        public List<string> Exits { get; }
-        public string Lobby { get; }
+    //public class Saloon : Building
+    //{
 
-        public TradingPost (int rooms, List<string> exits, string lobby)
-        {
-            Rooms = rooms;
-            Exits = exits;
-            Lobby = lobby;
-        }
-    }
-    public class Bank : Building
-    {
-        /// <summary> For Randomizer  </summary>
-        public int Rooms { get; }
-        public List<string> Exits { get; }
-        public string Lobby { get; }
+    //}
+    //public class TradingPost : Building
+    //{
+    //    public new List<string> Exits { get; }
+    //    public new string Lobby { get; }
 
-        public Bank (int rooms, List<string> exits, string lobby)
-        {
-            Rooms = rooms;
-            Exits = exits;
-            Lobby = lobby;
-        }
-    }
-    public class Church : Building
-    {
-        /// <summary> For Randomizer  </summary>
-        public int Rooms { get; }
-        public List<string> Exits { get; }
-        public string Lobby { get; }
+    //    public TradingPost(List<string> exits, string lobby)
+    //    {
+    //        Exits = exits;
+    //        Lobby = lobby;
+    //    }
+    //}
+    //public class Bank : Building
+    //{
+    //    public List<string> Exits { get; }
+    //    public string Lobby { get; }
 
-        public Church (int rooms, List<string> exits, string lobby)
-        {
-            Rooms = rooms;
-            Exits = exits;
-            Lobby = lobby;
-        }
-    }
-    public class Hotel : Building
-    {
-        /// <summary> For Randomizer  </summary>
-        public int Rooms { get; }
-        public List<string> Exits { get; }
-        public string Lobby { get; }
+    //    public Bank (List<string> exits, string lobby)
+    //    {
+    //        Exits = exits;
+    //        Lobby = lobby;
+    //    }
+    //}
+    //public class Church : Building
+    //{
+    //    public List<string> Exits { get; }
+    //    public string Lobby { get; }
 
-        public Hotel (int rooms, List<string> exits, string lobby)
-        {
-            Rooms = rooms;
-            Exits = exits;
-            Lobby = lobby;
-        }
-    }
-    public class Mine : Building
-    {
-        /// <summary> For Randomizer  </summary>
-        public int Rooms { get; }
-        public List<string> Exits { get; }
-        public string Lobby { get; }
+    //    public Church (List<string> exits, string lobby)
+    //    {
+    //        Exits = exits;
+    //        Lobby = lobby;
+    //    }
+    //}
+    //public class Hotel : Building
+    //{
+    //    public List<string> Exits { get; }
+    //    public string Lobby { get; }
 
-        public Mine (int rooms, List<string> exits, string lobby)
-        {
-            Rooms = rooms;
-            Exits = exits;
-            Lobby = lobby;
-        }
-    }
+    //    public Hotel (List<string> exits, string lobby)
+    //    {
+    //        Exits = exits;
+    //        Lobby = lobby;
+    //    }
+    //}
+    //public class Mine : Building
+    //{
+    //    public List<string> Exits { get; }
+    //    public string Lobby { get; }
+
+    //    public Mine (List<string> exits, string lobby)
+    //    {
+    //        Exits = exits;
+    //        Lobby = lobby;
+    //    }
+    //}
     
 }
--- PistolaDeOroGame/ProgramUI.cs
+++ PistolaDeOroGame/ProgramUI.cs
@@ -71,5 +71,48 @@ namespace PistolaDeOroGame
             Console.WriteLine("You finally arrived to the town and it is pretty dark. What would you like to do next?");
             Console.ReadKey();
         }
+
+        //The type of buildings are below:
+        public Building saloon = new Building(
+            "\n\n\nYou mosey into the saloon. The floor is sticky.\n\n\n" +
+            "You can either go to the CELLAR or head back to the STREET.\n",
+            new List<string> { "cellar", "street" }
+            );
+
+        public static Building tradingPost = new Building(
+            "\n\n\nYou are in the Trading Post. The goods sparkle enough to catch your eye. The clerk nods at you.\n\n\n" +
+            "You can either go to the BACKROOM or head back to the STREET.\n",
+            new List<string> { "backroom", "street" }
+            );
+
+        public Building hotel = new Building(
+            "\n\n\n Your investigation leads you to the hotel. The air is dry and mice scurry along the partially rotted floor.\n\n\n" +
+            "You can either go to the KITCHEN or head back to the STREET.\n",
+            new List<string> { "kitchen", "street" }
+            );
+
+        public Building bank = new Building(
+                    "\n\n\nWhile meandering you spot the bank. Your eyes turn into dollar signs. $$\n\n\n" +
+                    "You can either go into the VAULT or head back to the STREET.\n",
+                    new List<string> { "vault", "street" }
+                    );
+
+        public Building church = new Building(
+                    "\n\n\nEerily you edge toward the church. A raven croaks as you take your first step toward the pitch black of the santuary and you swear it says nevermore.\n\n\n" +
+                    "You can either go to the SANTUARY or head back to the STREET.\n",
+                    new List<string> { "santuary", "street" }
+                    );
+
+        public static Building street = new Building(
+            "\n\n\n You are on the street. The light from the moon shines off on your trusty pistol.\n\n\n" +
+            "You see several buildings.\n\n" +
+            "SALOON\n" +
+            "TRADING POST\n" +
+            "HOTEL\n" +
+            "BANK\n" +
+            "CHURCH", 
+            new List<string> { "saloon", "trading post", "hotel", "bank", "church" }
+            );
+
     }
 }
