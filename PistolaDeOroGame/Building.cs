6d377236ecf6af7a7f6f06f94c52ce687199dce6
Akinitas <82630851+Akinitas@users.noreply.github.com>
2021-05-12 14:16:18 -0400

    Building

---
+++ PistolaDeOroGame/Building.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PistolaDeOroGame
+{
+    public class Building
+
+    {
+        /// <summary> For Randomizer  </summary>
+        public int Rooms { get; } 
+        public List<string> Exits {get;}
+        public string Lobby { get; }
+
+        public Building(int rooms, List<string> exits, string lobby)
+        {
+            Rooms = rooms;
+            Exits = exits;
+            Lobby = lobby;
+        }
+    }
+}
