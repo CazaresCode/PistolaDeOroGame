7745a8521c581fc07e25e3c099ebbc2f981953af
Alexandro Cazares <CazaresCode@gmail.com>
2021-05-12 21:31:04 -0400

    updated the locations

--- PistolaDeOroGame/ProgramUI.cs
+++ PistolaDeOroGame/ProgramUI.cs
@@ -68,10 +68,11 @@ namespace PistolaDeOroGame
                 Console.ReadKey();
             }
 
-
             //They are at the street:
+            Console.Clear();
+            Console.WriteLine("You finally arrived to the town and it is pretty dark. You pull out your trusty lantern. Your research indicated the gold pistol is in this town.\n\n" +
+                "To begin your search, type: go [location]");
 
-            Console.WriteLine("You finally arrived to the town and it is pretty dark. What would you like to do next?");
             Building currentBuilding = street;
 
             bool alive = true;
@@ -85,97 +86,191 @@ namespace PistolaDeOroGame
 
                 // command to go or exit
 
-                if (command.StartsWith("go") || command.StartsWith("exit"))
+                if (command.StartsWith("go"))
                 {
                     bool foundExit = false;
                     foreach (string exit in currentBuilding.Exits)
                     {
-                        if(command.Contains(exit) && Buildings.ContainsKey(exit))
+                        if (command.Contains(exit) && Buildings.ContainsKey(exit))
                         {
                             currentBuilding = Buildings[exit];
                             foundExit = true;
                             break;
                         }
-                        if (!foundExit)
-                        {
-                            Console.WriteLine("At full tilt you high tail it to the street.");
-                        }
-                    } 
+                    }
+                    if (!foundExit)
+                    {
+                        Console.WriteLine("You are running out of time, oil, and perfurme. Better make a move. FAST!.");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Cheeseburger");
+                    Console.WriteLine("You know I can't help you. If you are lost, GO to the STREET");
                 }
-
-
                 // grab the pistol
-
-
-
             }
-
-
-            
             Console.ReadKey();
-
-
-
-
-
-
         }
 
         //The type of buildings are below:
+
+        //saloon
         public static Building saloon = new Building(
             "\n\n\nYou mosey into the saloon. The floor is sticky.\n\n\n" +
             "You can either go to the CELLAR or head back to the STREET.\n",
-            new List<string> { "cellar", "street" }
+            new List<string> { "cellar", "second floor", "street" }
             );
 
+        public static Building secondFloorSaloon = new Building(
+            "\n\n\nYou mosey into the saloon. The floor is sticky.\n\n\n" +
+            "You can only go back to the saloon. NO jumping out of windows...\n",
+            new List<string> { "saloon" }
+            );
+
+        public static Building cellarSaloon = new Building(
+            "\n\n\nYou step into a sticky situation. It is a little chilly... could it be the ghost who haunts this building?\n\n\n" +
+            "You can only go upstairs \n",
+            new List<string> { "saloon" }
+            );
+
+
+        //Trading Post
         public static Building tradingPost = new Building(
             "\n\n\nYou are in the Trading Post. The goods sparkle enough to catch your eye. The clerk nods at you.\n\n\n" +
             "You can either go to the BACKROOM or head back to the STREET.\n",
             new List<string> { "backroom", "street" }
             );
 
-        public static Building hotel = new Building(
-            "\n\n\n Your investigation leads you to the hotel. The air is dry and mice scurry along the partially rotted floor.\n\n\n" +
-            "You can either go to the KITCHEN or head back to the STREET.\n",
-            new List<string> { "kitchen", "street" }
+        public static Building storageRoomTradingPost = new Building(
+            "\n\n\nYou are in a small storage room, but it smells oddly nice in here. It might be the perfurme, actually. \n\n\n" +
+            "You can only go back to the lobby of the Trading Post.\n",
+            new List<string> { "trading post" }
             );
 
+        public static Building restroomTradingPost = new Building(
+            "\n\n\nNo judging for checking out the throne room.\n\n\n" +
+            "You can only go back to the lobby of the Trading Post.\n",
+            new List<string> { "trading post" }
+            );
+
+
+        //Mine
+
+        public static Building mine = new Building(
+            "\n\n\nThe mine shaft is quiet. It is still dark due to the lantern flickering. Do you really think it is a good idea to move another step forward?" +
+            "\n\nYou could STRAIGHT ahead into the darkness, turn RIGHT, or just turn back to the street.",
+            new List<string> { "street", "straight", "right" }
+            );
+
+        public static Building straightMine = new Building(
+            "\n\n\nYou fall into a hole. It goes on for miles, that you... die.\n\n\n\n\nI told you, didn't I?",
+            new List<string> { }
+            );
+
+        public static Building rightMine = new Building(
+            "\n\n\nYou see timesheets with names on it." +
+            "\n\nYou should go back the same way you came here.",
+            new List<string> { "mine" }
+            );
+
+        //hotel
+        public static Building hotel = new Building(
+            "\n\n\n Your investigation leads you to the hotel. The air is dry and mice scurry along the partially rotted floor.\n\n\n" +
+            "You can either go UPSTAIRS, to the KITCHEN or head back to the STREET.\n",
+            new List<string> { "upstairs", "kitchen", "street" }
+            );
+
+        public static Building upstairsHotel = new Building(
+            "\n\n\nYou creep upstairs. You notice only two doors are intact as the rest of the upstairs has been destroyed by time.\n\n\n" +
+            "You can either go to DOOR1, DOOR2 or head back to the HOTEL main floor.\n",
+            new List<string> { "door1", "door2", "hotel" }
+            );
+
+        public static Building door1Hotel = new Building(
+            "\n\n\nYou pull the door handle and the door creeks loudly. You take a step as you light up the lantern to see a mostly intact room. Including tub mirror and small bed. \n\n\n" +
+            "You can either go to DOOR2 or head back to the HOTEL lobby.\n",
+            new List<string> { "door2", "hotel" }
+            );
+
+        public static Building door2Hotel = new Building(
+            "\n\n\n You pull on the handle breaking it off. Using the lanturn you peer through the door hole and see and red glowing eye angrily looking back. You turn to leave pale as a ghost. \n\n\n" +
+            "You can either go search the HOTEL lobby or head back to DOOR1.\n",
+            new List<string> { "hotel", "door1" }
+            );
+
+        public static Building kitchenHotel = new Building(
+            "\n\n\n Swearing you can smell food you head toward the kitchen. You see the woodburning stove flicker then fire explodes out its doors.\n\n\n" +
+            "You can either go UPSTAIRS or head back to the HOTEL lobby.\n",
+            new List<string> { "upstairs", "hotel" }
+            );
+
+        //bank
         public static Building bank = new Building(
-                    "\n\n\nWhile meandering you spot the bank. Your eyes turn into dollar signs. $$\n\n\n" +
-                    "You can either go into the VAULT or head back to the STREET.\n",
-                    new List<string> { "vault", "street" }
-                    );
+            "\n\n\nWhile meandering you spot the bank. Your eyes turn into dollar signs. $$\n\n\n" +
+            "You can either go into the VAULT, TELLER WINDOW, or head back to the STREET.\n",
+            new List<string> { "vault", "teller window", "street" }
+            );
 
+        public static Building tellerWindowBank = new Building(
+            "\n\n\nYou slowly edge toward the derelict TELLER WINDOW to your left. You search through all the drawers.\n\n\n" +
+            "You can either go into the VAULT or head back to the BANK lobby.\n",
+            new List<string> { "vault", "bank" }
+            );
+
+        public static Building vaultBank = new Building(
+            "\n\n\nYou excitedly head towards the vault. Your antipation that this will be the final destination of the pistol is rising. You search every inch of the vault.\n\n\n" +
+            "You can only go back to the BANK lobby.\n",
+            new List<string> { "bank" }
+            );
+
+        //church
         public static Building church = new Building(
-                    "\n\n\nEerily you edge toward the church. A raven croaks as you take your first step toward the pitch black of the santuary and you swear it says nevermore.\n\n\n" +
-                    "You can either go to the SANTUARY or head back to the STREET.\n",
-                    new List<string> { "santuary", "street" }
-                    );
+            "\n\n\nEerily you edge toward the church. A raven croaks as you take your first step toward the pitch black of the santuary and you swear it says nevermore.\n\n\n" +
+            "You can take another step forward towards the SANCTUARY or head back to the STREET.\n",
+            new List<string> { "sanctuary", "street" }
+            );
 
+        public static Building sanctuaryChurch = new Building(
+            "\n\n\nYour foot never reaches solid ground. You fall 100 feet into a dark solitary room closed off from the mine breaking your neck.\n\n\n" +
+            "Again, I tried to warn you... Haven't you read Edgar Allen Poe?!\n\n\n\n\n",
+            new List<string> { }
+            );
+
+        //street
         public static Building street = new Building(
-            "\n\n\n You are on the street. The light from the moon shines off on your trusty pistol.\n\n\n" +
+            "\n\nYou are on the STREET.\n\n\n" +
             "You see several buildings.\n\n" +
             "SALOON\n" +
             "TRADING POST\n" +
             "HOTEL\n" +
             "BANK\n" +
-            "CHURCH", 
-            new List<string> { "saloon", "trading post", "hotel", "bank", "church" }
-            );
+            "CHURCH\n" +
+            "MINE",
+            new List<string> { "saloon", "trading post", "hotel", "bank", "church", "mine" });
 
-
+        // Disctionary for the locations
         public readonly Dictionary<string, Building> Buildings = new Dictionary<string, Building>
         {
             {"street", street},
             {"saloon", saloon},
+            {"second floor", secondFloorSaloon },
+            {"cellar", cellarSaloon },
             {"trading Post", tradingPost},
+            {"storage room", storageRoomTradingPost },
+            {"restroom", restroomTradingPost },
+            {"mine", mine },
+            {"right", rightMine },
+            {"straight", straightMine },
             {"hotel", hotel},
+            {"kitchen", kitchenHotel},
+            {"upstairs", upstairsHotel},
+            {"door1", door1Hotel},
+            {"door2", door2Hotel},
+            {"church", church},
+            {"sanctuary", sanctuaryChurch },
             {"bank", bank},
-            {"Church", church},
-            };
+            {"vault", vaultBank},
+            {"teller window", tellerWindowBank}
+        };
     }
 }
